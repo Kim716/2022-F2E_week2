edabbd40fe85c9c2b29efa3a980b6dc050d76148
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-20 12:13:03 +0800

    store file in local storage

---
+++ pdf.js
@@ -0,0 +1,14 @@
+"use strict";
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
+
+// --- DATA --- //
+const pdfFile = JSON.parse(localStorage.getItem("file"));
+
+// --- DOM nodes --- //
+
+// --- FUNCTIONS --- //
+
+// --- EVENT LISTENER --- //
+
+// --- EXECUTE --- //
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+const base64Prefix = "data:application/pdf;base64,";
 
 // --- DOM nodes --- //
 const startBtn = document.querySelector(".start-btn");
@@ -16,6 +17,27 @@ function showModal(modal) {
 // FN-2
 function closeModal(modal) {
   modal.classList.add("hide");
+}
+
+// FN-3 轉檔
+function readBlob(blod) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => resolve(reader.result));
+    reader.addEventListener("error", reject);
+    // 以 data: URL 格式（base64 編碼）的字串來表示讀入的資料內容。
+    reader.readAsDataURL(blod);
+  });
+}
+
+// FN-4 儲存 PDF 到 local
+async function storePDFdata(pdfData) {
+  // 將檔案處理成 base64
+  pdfData = await readBlob(pdfData);
+
+  // 刪除 base64 的前綴，並解碼
+  const data = atob(pdfData.substring(base64Prefix.length));
+  localStorage.setItem("file", JSON.stringify(data));
 }
 
 // --- EVENT LISTENER --- //
@@ -36,7 +58,10 @@ outerModal.addEventListener("click", (e) => {
   }
 });
 
-// EL-4 當 PDF 上傳完畢，跳轉頁面
-uploadPDF.addEventListener("change", () => {
+// EL-4 當 PDF 上傳完畢，將檔案存在local，並跳轉頁面
+uploadPDF.addEventListener("change", async (e) => {
+  if (e.target.files[0] === undefined) return;
+
+  await storePDFdata(e.target.files[0]);
   window.location.assign("./pdf.html");
 });
