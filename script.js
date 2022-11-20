b19dbbfd02b98fda5aeaa02410397b3cca4ecc31
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-20 11:19:07 +0800

    pdf page visualize

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+// --- DOM nodes --- //
+const startBtn = document.querySelector(".start-btn");
+const startPageModal = document.querySelector(".modal-background");
+const closeBtn = document.querySelector(".close-btn");
+const outerModal = document.querySelector(".modal-background");
+const uploadPDF = document.querySelector("#uploadPDF");
+
+// --- FUNCTION --- //
+// FN-1
+function showModal(modal) {
+  modal.classList.remove("hide");
+}
+
+// FN-2
+function closeModal(modal) {
+  modal.classList.add("hide");
+}
+
+// --- EVENT LISTENER --- //
+// EL-1
+startBtn.addEventListener("click", (e) => {
+  showModal(startPageModal);
+});
+
+// EL-2
+closeBtn.addEventListener("click", (e) => {
+  closeModal(startPageModal);
+});
+
+// EL-3 點擊 modal 外圍可以關掉
+outerModal.addEventListener("click", (e) => {
+  if (!e.target.matches(".modal")) {
+    closeModal(startPageModal);
+  }
+});
+
+// EL-4 當 PDF 上傳完畢，跳轉頁面
+uploadPDF.addEventListener("change", () => {
+  window.location.assign("./pdf.html");
+});
